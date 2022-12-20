15ba66cf4faed1fddee91bc20a0b20db608ef07a
Ryu-dongwoo <cjstkehddn11@naver.com>
2022-12-20 15:17:03 +0900

    20221220

---
+++ syntax/outputs.tf
@@ -0,0 +1,13 @@
+# 변수값 출력
+output "number_output" {
+  value = var.number_example
+}
+
+
+output "string_output" {
+  value = var.string_example
+}
+
+output "list_output" {
+  value = var.list_example
+}
